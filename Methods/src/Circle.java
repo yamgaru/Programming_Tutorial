9160292d1b2b6b7f1b21d169963aaf7e3862fd5f
Nana <nana.d@hotmail.de>
2016-10-28 14:28:36 +0200

    added exercises for methods

---
+++ Methods/src/Circle.java
@@ -0,0 +1,5 @@
+
+public class Circle {
+	//todo: add some methods to compute a circle's diameter, circumference and area.
+	//print some outputs to confirm you methods are working right.
+}
---
+++ Methods/src/Prime.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+
+public class Prime {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		//todo: write a method which tells you if a given number is a prime or not
+		
+	}
+	
+}
