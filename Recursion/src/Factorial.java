659afae1c58f6f852bfec0c6b99c7a6ed8b74e8f
Nana <nana.d@hotmail.de>
2016-10-28 15:01:17 +0200

    added exercises for recursion

---
+++ Recursion/src/Factorial.java
@@ -0,0 +1,4 @@
+
+public class Factorial {
+	//todo: implement a recursive functions whicht gives you the factorial value of a given number
+}
---
+++ Recursion/src/Fibonacci.java
@@ -0,0 +1,5 @@
+
+public class Fibonacci {
+	//todo: implement a recursive function which takes an integer value as inputs and outputs the corresponding Fibonacci number.
+	//if you don't know what Fibonacci numbers are you can read it here: https://en.wikipedia.org/wiki/Fibonacci_number
+}
