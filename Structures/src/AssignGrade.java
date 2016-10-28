b9035cf6bc4f783a2630c1135de080ff55978eff
Nana <nana.d@hotmail.de>
2016-10-28 13:56:40 +0200

    added Structures exercises

---
+++ Structures/src/AssignGrade.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+//if-else
+
+public class AssignGrade {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		//todo: when executing the program you will be ask to write a score.
+		//The program converts the score then into a grade and prints that grade (1.0, 1.3, 1.7, ...)
+		//Note that the student passed when the student gets a score of 50.0 points (4.0)
+		
+	}
+}
