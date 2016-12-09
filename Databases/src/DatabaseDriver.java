7a5faf7350b31151d627e364955ff23e2a3cbaf4
Nana <nana.d@hotmail.de>
2016-12-09 14:06:04 +0100

    added Databases exercise

---
+++ Databases/src/DatabaseDriver.java
@@ -0,0 +1,11 @@
+
+public class DatabaseDriver {
+	
+	//todo: test your methods from the DatabaseHelper-class here and print the results
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Databases/src/DatabaseHelper.java
@@ -0,0 +1,15 @@
+
+public class DatabaseHelper {
+
+	//todo: You want to create a social network.
+	//For that you need a database which has the following properties:
+	//- create a table for 'Users'
+	//	-> each user has to have a unique nickname, an eMail adress and a password
+	//	-> users can post messages which are visible to everyone
+	//- two users can be friends. create a table 'Friends'
+	//Add the following methods to your DatabaseHelper class:
+	//- get all Nicknames from users
+	//- get the last 5 posted messages from a selected user
+	//- get the last 3 posted messages of all friends of one user
+
+}
