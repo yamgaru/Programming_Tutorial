c7a66297d16f243c5dc2bec6adafc67e1bab3c79
Nana <nana.d@hotmail.de>
2016-12-16 15:29:31 +0100

    better Generics exercise

---
+++ Generics/src/Pair.java
@@ -0,0 +1,7 @@
+
+public class Pair {
+	//todo: In Java it is not possible to have two return values. Luckily you can use Generics to solve that problem.
+	//Change this class to a Generic where you can have a pair of two Variables even with different data types.
+	//Write a constructor for this class to set those values and also getter and setter methods to retrieve them and
+	//test your Methods in a driver-class
+}
