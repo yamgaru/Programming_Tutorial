faad15304f447dbee63b244cd1d3e84acfcd1a68
Nana <nana.d@hotmail.de>
2017-06-28 14:39:03 +0200

    Added InputOutput Exercise

---
+++ InputOutput/src/FileIO.java
@@ -0,0 +1,8 @@
+
+public class FileIO {
+
+	//todo: Create a file with at least 5 lines of arbitrary text
+	//create a second file and copy the first and the last word of every line of your arbitrary text
+	//donot forget to save your file
+	
+}
---
+++ InputOutput/src/ImageIO.java
@@ -0,0 +1,11 @@
+
+public class ImageIO {
+	
+	//todo: load the file image.jpg into this class
+	//change this image into grayscale and save it into a new image
+	//use the following color conversion for every pixel in your image:
+	//red *= 0.299
+	//green *= 0.587
+	//blue *= 0.114
+
+}
