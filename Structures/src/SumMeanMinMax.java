cda6dfaef7451a6eb177c2dd1c7b5d179167f1fc
Nana <nana.d@hotmail.de>
2018-04-13 11:31:47 +0200

    updated structures exercise

---
+++ Structures/src/SumMeanMinMax.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class SumMeanMinMax {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		
+		/*
+		 * Todo: Print the sum, mean, max Value and min Value of a, b and c.
+		 * */
+		
+
+	}
+
+}
