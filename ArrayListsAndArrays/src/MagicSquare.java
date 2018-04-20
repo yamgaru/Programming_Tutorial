c555a6cc0976c35cc23ac11e19edc28e8b945d7f
Nana <nana.d@hotmail.de>
2018-04-20 14:10:30 +0200

    reorganized arraylists and arrays exercise

---
+++ ArrayListsAndArrays/src/MagicSquare.java
@@ -0,0 +1,9 @@
+//Bonus 2D Arrays
+public class MagicSquare {
+	/*
+	 * Todo: A magic square is a quadratic matrix where the sums of each row and
+	 * each column are equal. All possible numbers from 1 to (side length)^2 have to occur
+	 * in the square. Write a method that takes a 2D Array as input and checks if it is a magic
+	 * square or not.
+	 * */
+}
---
+++ ArrayListsAndArrays/src/MatrixMultiplication.java
@@ -0,0 +1,9 @@
+//2D Arrays
+public class MatrixMultiplication {
+	
+	//Todo: Each 2D Array can be treated like a matrix.
+	//Write a method which takes two matrices as input and returns the result.
+	//Write a main-method where you call your created method for matrix multiplication
+	//and print the result properly
+	
+}
