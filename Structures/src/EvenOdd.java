89c8fd462d350c935f630232e7ed90f0ad6f066b
Konka Yamini <yamini.konka@gmail.com>
2018-05-18 20:26:53 +0530

    progress so far

--- Structures/src/EvenOdd.java
+++ Structures/src/EvenOdd.java
@@ -6,6 +6,15 @@ public class EvenOdd {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		int X=scan.nextInt();
+		int number=X%2;
+		if (number==0) {
+				System.out.println("even");
+		} else if (number==1) {
+			System.out.println("odd");
+		}
+			
+			
 		
 		//todo: The program takes a number as input.
 		//It then goes through all integer values till reaching that value and prints all the odd ones.
--- Structures/src/SumMeanMinMax.java
+++ Structures/src/SumMeanMinMax.java
@@ -13,6 +13,8 @@ public class SumMeanMinMax {
 		int b = scan.nextInt();
 		int c = scan.nextInt();
 		
+		System.out.println("You have entered: " + a + ", " + b + ", " + c);
+		
 		/*
 		 * Todo: Print the sum, mean, max Value and min Value of a, b and c.
 		 * */
